4f698d76deb01f6ae87522bae70628de372f8b82
raj <106125091@nitt.edu>
2026-03-19 01:22:27 +0530

    updated the physics

--- backend/core/physics_engine.py
+++ backend/core/physics_engine.py
@@ -5,7 +5,8 @@ J2 = np.float64(0.00108263) ### unitless
 RE = np.float64(6378.137) ### km radius of earth
 
 eps = 1e-9  ####### softening (no zerodivision)
-
+I_sp =  np.float64(300.00)
+g0 = np.float64(0.00980665) # km/s^2
 
 
 ###########################
@@ -25,7 +26,8 @@ class physicsengine:
         ####
         self.ids = np.empty(self.total_objects, dtype=object)
         
-        
+        self.idtoind = {}
+        self.issate = np.zeros(self.total_objects, dtype=bool)
         
         ### kind of metadata
         self.masses = np.ones(self.total_objects, dtype=np.float64) * 550.0 
@@ -89,3 +91,80 @@ class physicsengine:
         
         self.state_matrix +=   (dt/6.0) * (k1+2*k2+2*k3+k4) 
         self.cooldowns = np.maximum(0, self.cooldowns-dt)
+        
+    # def delm(self, )
+    def burn(self, i,  delv):
+        ##### 15 m/s 0.015 km/s
+        if (np.linalg.norm(delv) <= 0.015) and (self.cooldowns[i]<=0):
+            self.state_matrix[i, 3:6]  += delv
+            self.cooldowns[i] =600
+            powerthing = (-np.linalg.norm(delv))/(I_sp*g0)
+            delm = self.masses[i]*(1-np.exp(powerthing))
+            if (self.masses[i] - delm >= 500.0):
+                self.masses[i] -= delm
+                
+    def move(self, bigt): 
+        ### to reduceb thje erron
+        ## we divide in 5 second chunks
+        n = bigt//5
+        rem = bigt -n
+        for i in range(n):
+            self.onestep(5.0)
+        self.onestep(rem)
+        
+        
+        
+        
+    def ingest(self, payload):
+        objects = payload.get("objects", [])
+        
+        for obj in objects:
+            oid = obj["id"]
+            
+            ############for new things
+            if oid not in self.idtoind:
+                id = len(self.idtoind)
+                if id >= self.total_objects:
+                    continue #### its says constaret
+                
+                self.idtoind[oid] = idx
+                self.ids[idx] = oid
+                self.issate[idx] = (obj["type"] == "SAT") # for now keey "SAT"$#$$$$$#######
+            
+            idx = self.idtoind[oid]
+            self.state_matrix[idx, 0] = obj["r"]["x"]
+            self.state_matrix[idx, 1] = obj["r"]["y"]
+            self.state_matrix[idx, 2] = obj["r"]["z"]
+            self.state_matrix[idx, 3] = obj["v"]["x"]
+            self.state_matrix[idx, 4] = obj["v"]["y"]
+            self.state_matrix[idx, 5] = obj["v"]["z"]
+            
+            
+            
+            
+    def rtntoeci(self, i):
+        ### its to convert the rtn time to earth centre
+        r = self.state_matrix[i, 0:3]
+        v = self.state_matrix[i, 3:6] ## obv
+        
+        rn = np.linalg.norm(r)
+        R = r/(rn + eps)  ## rn cant be zero anywya
+        
+        L = np.cross(r, v)
+        Ln = np.linalg.norm(L)
+        
+        N = L/(Ln+eps)
+
+        T = np.cross(N,R)
+        
+        return np.column_stack((R,T,N))        
+    
+    
+    def burnrtn(self,i, delvrtn):
+        ## input giben in rtn
+        rm = self.rtntoeci(i)
+        delv = rm@ np.array(delvrtn)
+        
+        self.burn(i, delv)
+        
+    
